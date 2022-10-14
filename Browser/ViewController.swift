64eafb8c1ab40ba13c0cbb44b146a57d1782272d
sameddagl <samedddagl@gmail.com>
2022-10-14 16:39:57 +0300

    Initial Commit

---
+++ Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by Samed Dağlı on 14.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
